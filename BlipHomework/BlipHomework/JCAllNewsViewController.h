f6cf0bc3f68c3e1b4aeee9c0a6e990e72094b847
jpcarreira <jpmavcarreira@gmail.com>
2013-12-20 00:35:49 +0000

    added AllNewsTableViewController and embebbed in NavigationController

---
+++ BlipHomework/BlipHomework/JCAllNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCAllNewsViewController.h
+//  BlipHomework
+//
+//  Created by João Carreira on 20/12/13.
+//  Copyright (c) 2013 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAllNewsViewController : UITableViewController
+
+@end
